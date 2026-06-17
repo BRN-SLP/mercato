e288f7bc8734a6cafeb0dea9b2555c7f2df65de3
BRN-SLP <v.khylynskyi@gmail.com>
2026-06-17 03:36:50 +0300

    i18n(lv): register Latvian locale

--- apps/web/src/i18n/routing.ts
+++ apps/web/src/i18n/routing.ts
@@ -25,7 +25,7 @@ import { defineRouting } from "next-intl/routing";
  * explicit.
  */
 export const routing = defineRouting({
-  locales: ["en", "uk", "es", "pt-BR", "de", "fr", "pl", "ro", "it", "tr", "et", "lt"],
+  locales: ["en", "uk", "es", "pt-BR", "de", "fr", "pl", "ro", "it", "tr", "et", "lt", "lv"],
   defaultLocale: "en",
   localePrefix: "as-needed",
 });
