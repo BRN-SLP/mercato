6b858ac3019bed8ceeb278148cd16c4ed5507c7b
BRN-SLP <v.khylynskyi@gmail.com>
2026-06-17 03:35:49 +0300

    i18n(ro): register Romanian locale

--- apps/web/src/i18n/routing.ts
+++ apps/web/src/i18n/routing.ts
@@ -25,7 +25,7 @@ import { defineRouting } from "next-intl/routing";
  * explicit.
  */
 export const routing = defineRouting({
-  locales: ["en", "uk", "es", "pt-BR", "de", "fr", "pl"],
+  locales: ["en", "uk", "es", "pt-BR", "de", "fr", "pl", "ro"],
   defaultLocale: "en",
   localePrefix: "as-needed",
 });
