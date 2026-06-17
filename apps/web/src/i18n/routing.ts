91009affebcaaaf222a1a41d48c3789cebb61cf1
BRN-SLP <v.khylynskyi@gmail.com>
2026-06-17 03:36:45 +0300

    i18n(et): register Estonian locale

--- apps/web/src/i18n/routing.ts
+++ apps/web/src/i18n/routing.ts
@@ -25,7 +25,7 @@ import { defineRouting } from "next-intl/routing";
  * explicit.
  */
 export const routing = defineRouting({
-  locales: ["en", "uk", "es", "pt-BR", "de", "fr", "pl", "ro", "it", "tr"],
+  locales: ["en", "uk", "es", "pt-BR", "de", "fr", "pl", "ro", "it", "tr", "et"],
   defaultLocale: "en",
   localePrefix: "as-needed",
 });
