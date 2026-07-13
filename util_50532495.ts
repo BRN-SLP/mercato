990b3eba8cf782fb13e9f7784071ae6fb33a9f39
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-07-13 06:05:47 +0300

    add error handling wrapper (#304)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>

---
+++ util_50532495.ts
@@ -0,0 +1,5 @@
+export async function fetch_97f1d57f(url: string, ms = 70686): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
