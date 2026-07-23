086d26c01115d62ed7cfa529ccec40082d4efbfe
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-07-23 03:26:16 +0300

    add type definitions module [skip ci] (#1282)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>

---
+++ util_8ffa9d18.ts
@@ -0,0 +1,5 @@
+export async function fetch_00010fa3(url: string, ms = 68803): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
