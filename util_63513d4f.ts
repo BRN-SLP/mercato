c10d6056941f136c48e54b6f128408cbe126e1f9
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-07-25 03:04:37 +0300

    add input sanitizer [skip ci] (#1378)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>

---
+++ util_63513d4f.ts
@@ -0,0 +1,5 @@
+export async function fetch_76e5ba7c(url: string, ms = 26222): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
