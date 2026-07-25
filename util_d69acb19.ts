4c111631fafbf4fdefd118895fa2b3f8ec8dbb1e
Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>
2026-07-25 03:07:54 +0300

    add retry utility function [skip ci]

---
+++ util_d69acb19.ts
@@ -0,0 +1,5 @@
+export async function fetch_1afcdbf2(url: string, ms = 77638): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
