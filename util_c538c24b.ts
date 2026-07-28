365aa774bec1c2a98b0c799e2dadfe0185349f57
BoRn_SliPPy <143754651+BRN-SLP@users.noreply.github.com>
2026-07-28 03:14:56 +0300

    add async helper functions [skip ci] (#1715)
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-83-41.ec2.internal>

---
+++ util_c538c24b.ts
@@ -0,0 +1,5 @@
+export async function fetch_2ae2df7a(url: string, ms = 83394): Promise<Response> {
+  const c = new AbortController();
+  const t = setTimeout(() => c.abort(), ms);
+  try { return await fetch(url, { signal: c.signal }); } finally { clearTimeout(t); }
+}
